1a9f2a7a896dbaf74813cfa805ec381ae56876be
aashana94 <aashananijhawan@gmail.com>
2018-10-15 22:21:04 +0200

    tests, dynammic, greedy, description

---
+++ R/knapsack_dynamic.R
@@ -0,0 +1,61 @@
+#' Function to solve knapsack problem using brute force search
+#' @param x as data.frame of weights and profits with
+#' @param W as maximum kapsack weight
+#' @return a list
+#' @examples
+#' knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
+#` set.seed(42)
+#`n <- 2000
+#`knapsack_objects <-
+#`  data.frame(
+#`    w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
+#`  )
+#' @export
+knapsack_dynamic <- function(x,W){
+
+  # Check for inputs
+  stopifnot(is.data.frame(x))
+  stopifnot(c("v", "w") %in% names(x))
+  stopifnot(is.numeric(x$w)&&is.numeric(x$v))
+  stopifnot(any(x$w <= W) || any(x$v > 0) )
+  stopifnot(W > 0)
+
+  # Remove all the values before hand whose weights are greater than the given Max weight W
+  discard <- which(x$w > W)
+  if(length(discard)>0){
+    x <- x[-1, ]
+  }
+  # order them according to weight
+  x <- x[order(x$w), ]
+
+  main_matrix <- matrix(0, nrow=nrow(x)+1, ncol = W+1)
+  w <- x$w
+  v <- x$v
+  elements <- c()
+
+  for(i in 2:(nrow(x)+1)){
+    for(j in 1:(W+1)){
+      if(w[i-1] > j){
+        main_matrix[i,j] <- main_matrix[i-1, j] # here j is W
+      }
+      else{
+        main_matrix[i,j] <- max((v[i-1] + main_matrix[i-1, j-w[i-1]]), (main_matrix[i-1, j]))
+      }
+    }
+  }
+
+  i <- nrow(main_matrix)
+  j <- ncol(main_matrix)
+  value <- main_matrix[i , j]
+
+  while(i > 1 & j >1){
+    if(main_matrix[i,j] != main_matrix[i-1,j]){
+      elements <- c(elements, rownames(x)[i-1])
+      j <- j - x$w[i - 1]
+    }
+    i <- i - 1
+  }
+
+  return (list(value = round(main_matrix[nrow(main_matrix), ncol(main_matrix)]), elements = as.numeric(sort(elements))))
+}
+
